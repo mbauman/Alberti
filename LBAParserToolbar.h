3ddabab4d29e8e89d157d47d6c821580d2ec75fe
Matt Bauman <mbauman@gmail.com>
2010-01-24 19:37:54 -0600

    Added stubs for the custom view controllers, and laid them out in the Collect Window.

---
+++ LBAParserToolbar.h
@@ -0,0 +1,16 @@
+//
+//  LBAParserToolbar.h
+//  Alberti
+//
+//  Created by Matt Bauman on 1/24/10.
+//  Copyright 2010 Matt Bauman. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface LBAParserToolbar : NSView {
+
+}
+
+@end
---
+++ LBAParserToolbar.m
@@ -0,0 +1,26 @@
+//
+//  LBAParserToolbar.m
+//  Alberti
+//
+//  Created by Matt Bauman on 1/24/10.
+//  Copyright 2010 Matt Bauman. All rights reserved.
+//
+
+#import "LBAParserToolbar.h"
+
+
+@implementation LBAParserToolbar
+
+- (id)initWithFrame:(NSRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect {
+    // Drawing code here.
+}
+
+@end
