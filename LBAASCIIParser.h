5bc899e9d8e56913b1d6595fe2ffb87098b6dc54
Matt Bauman <mbauman@gmail.com>
2010-01-29 22:03:06 -0600

    Created a concrete subclass of LBAParser: LBAASCIIParser

---
+++ LBAASCIIParser.h
@@ -0,0 +1,16 @@
+//
+//  LBAASCIIParser.h
+//  Alberti
+//
+//  Created by Matt Bauman on 1/29/10.
+//  Copyright 2010 Northwestern University. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "LBAParser.h"
+
+@interface LBAASCIIParser : LBAParser {
+
+}
+
+@end
---
+++ LBAASCIIParser.m
@@ -0,0 +1,18 @@
+//
+//  LBAASCIIParser.m
+//  Alberti
+//
+//  Created by Matt Bauman on 1/29/10.
+//  Copyright 2010 Northwestern University. All rights reserved.
+//
+
+#import "LBAASCIIParser.h"
+
+
+@implementation LBAASCIIParser
+
+- (NSString *)parse:(LBAData *)data {
+	
+}
+
+@end
